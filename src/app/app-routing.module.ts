0d6397d59f18fddea33e2d5343634d26e3dec136
Otavio Barros <otaviobarros777@gmail.com>
2021-12-09 18:16:07 -0300

    inicio + modulo companies + rota (18:16)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { CompaniesComponent } from './companies/companies.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'companies',
+    pathMatch: 'full'
+  },
+  {
+    path: 'companies',
+    component: CompaniesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
