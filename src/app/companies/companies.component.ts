fc3696a475f24f1a67e98f690eccb275ed48469d
Otavio Barros <otaviobarros777@gmail.com>
2021-12-09 18:55:27 -0300

    uso do primeiro serviço get + css view (18:55)

--- src/app/companies/companies.component.ts
+++ src/app/companies/companies.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CompanyServiceService } from '../services/company-service.service';
+import { Company } from '../models/company';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-companies',
@@ -7,11 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompaniesComponent implements OnInit {
 
-  
 
-  constructor() { }
+  // Cria variável company sendo uma Company;
+  company = {} as Company;
+
+  // Cria variável companies sendo uma Company Array
+  companies = [] as Company[];
+
+  constructor(private companyService: CompanyServiceService) { }
 
   ngOnInit(): void {
+    // Chama o método getComapanies() ao inicializar o módulo
+    this.getCompanies();
+  }
+
+  // Criando método getCompanies que busca de nosso serviço CompanyServiceService
+  getCompanies() {
+    this.companyService.getCompanies().subscribe((companies: Company[]) => {
+      this.companies = companies
+    })
   }
 
 }
