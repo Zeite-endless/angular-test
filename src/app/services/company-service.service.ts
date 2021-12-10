aa3d61296248f6ac92f30f31cefacfdf07d14ee7
Otavio Barros <otaviobarros777@gmail.com>
2021-12-09 22:11:40 -0300

    criação de modais para att de dados (22:11)

--- src/app/services/company-service.service.ts
+++ src/app/services/company-service.service.ts
@@ -18,6 +18,43 @@ export class CompanyServiceService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   }
 
+  saveCompany(company: Company): Observable<Company>{
+    console.log(company)
+
+    // fetch('https://api.helena.app/test/api/Company', {
+    //   method: 'POST',
+    //   body: company
+    // })
+
+    return this.httpClient.post<Company>(this.url, company, this.httpOptions).pipe(
+      retry(2),
+      catchError(this.handleError)
+    )
+  }
+
+  // UPDATE de uma companhia existente
+  updateCompany(company: Company): Observable<Company> {
+
+    return this.httpClient.put<Company>(this.url + '/' + company.id, company, this.httpOptions).pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }
+
+  // DELETE de uma companhia existente
+  deleteCompany(company: Company): Observable<Company> {
+    
+    // fetch(`https://api.helena.app/test/api/Company/${company.id}`, {
+    //   method: 'DELETE',
+    //   redirect: 'follow'
+    // })
+
+    return this.httpClient.delete<Company>(this.url + '/' + company.id, this.httpOptions).pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }
+
   // GET de todas as companhias
   getCompanies(): Observable<Company[]> {
     return this.httpClient.get<Company[]>(this.url).pipe(
@@ -35,28 +72,7 @@ export class CompanyServiceService {
   }
 
   // POST de uma nova comapnhia
-  saveCompany(company: Company): Observable<Company> {
-    return this.httpClient.post<Company>(this.url, JSON.stringify(company), this.httpOptions).pipe(
-      retry(2),
-      catchError(this.handleError)
-    )
-  }
-
-  // UPDATE de uma companhia existente
-  updateCompany(company: Company): Observable<Company> {
-    return this.httpClient.put<Company>(this.url + '/' + company.id, JSON.stringify(company), this.httpOptions).pipe(
-      retry(1),
-      catchError(this.handleError)
-    )
-  }
-
-  // DELETE de uma companhia existente
-  deleteCompany(company: Company){
-    return this.httpClient.delete<Company>(this.url + '/' + company.id, this.httpOptions).pipe(
-      retry(1),
-      catchError(this.handleError)
-    )
-  }
+  
 
   // Método que trata os erros
   handleError(error: HttpErrorResponse) {
